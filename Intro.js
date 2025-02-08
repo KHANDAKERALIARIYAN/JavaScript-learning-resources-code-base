356ebecfc233a636d47bc0cc6641dea91d8ee97a
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2025-02-08 23:05:23 +0600

    Add files via upload

---
+++ Intro.js
@@ -0,0 +1,20 @@
+// alert("Hello World");
+
+// console.log("Hello I am Ariyan");
+// console.log("Hey i am console");
+// console.log("Code is running");
+// console.log("Code is also running");
+// console.log("Code is again running");
+
+// var a=prompt("Enter your number?");
+// console.log("Your number is: "+a);
+
+// var isTrue=confirm("Are you sure you want to burn your computer?");
+
+// if(isTrue){
+//     console.log("Your computer is burning");
+// }
+
+// else{
+//     console.log("Your computer is safe");
+// }
