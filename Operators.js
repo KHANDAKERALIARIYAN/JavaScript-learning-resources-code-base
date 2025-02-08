0104aefa7ad743bb9977cdc56acf5370ebc21068
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2025-02-08 22:48:34 +0600

    Add files via upload

---
+++ Operators.js
@@ -0,0 +1,73 @@
+// Expressions and comments
+
+// Comment
+// single line Comment
+// console.log("Hello World");
+
+// multi line Comment
+/*
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+*/
+
+// operators
+
+// Arithmetic operator
+
+// let a=10;
+// let b=2;
+
+// console.log("a + b =", a + b);
+// console.log("a - b =", a - b);
+// console.log("a * b =", a * b);
+// console.log("a / b =", a / b);
+// console.log("a % b =", a % b);
+// console.log("a ** b =", a ** b);
+
+// a++
+// console.log("a =", a);
+// a--
+// console.log("a =", a);
+
+// Assingment operator
+
+// let a=10;
+// console.log(a);
+// a+=2;
+// console.log(a);
+// a-=2;
+// console.log(a);
+// a*=2;
+// console.log(a);
+// a/=2;
+// console.log(a);
+// a%=2;
+// console.log(a);
+// a**=2;
+// console.log(a);
+
+// Comparison operator == != >= <= > < === !== 
+
+// let a=10;
+// let b=20;
+// console.log("a == b =", a == b);
+// console.log("a != b =", a != b);
+// console.log("a >= b =", a >= b);
+// console.log("a <= b =", a <= b);
+// console.log("a > b =", a > b);
+// console.log("a < b =", a < b);
+// console.log("a === b =", a === b);
+// console.log("a !== b =", a !== b);
+// console.log("a === 10 =", a === 10);
+// console.log("a !== 10 =", a !== 10);
+
+// Logical Operator
+
+// let a=10;
+// let b=2;
+
+// console.log("a && b =", a && b);
+// console.log("a || b =", a || b);
+// console.log("!a" ,!a);
+// console.log("!b" ,!b);
