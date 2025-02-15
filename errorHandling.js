4ee07b70de1207bfb9e6af459ba53393c2b472f7
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2025-02-16 00:46:05 +0600

    Update errorHandling.js

--- errorHandling.js
+++ errorHandling.js
@@ -1,2 +1,48 @@
-let a = prompt("Enter the 1st number?");
-let b = prompt("Enter the 2nd number?");
+// 1.
+
+// let a = prompt("Enter the 1st number?");
+
+// let b = prompt("Enter the 2nd number?");
+
+// if(isNaN(a) || isNaN(b)){
+//     throw SyntaxError("Invalid input");
+// }
+
+// let sum =parseInt(a) + parseInt(b);
+// let x=6;
+// try {
+//     console.log("Sum is: "+sum*x);
+
+// } catch (error) {
+//     console.log("Error occured");
+//     // console.log("Error: "+error.message);
+// } finally{
+//     console.log("Finally files are closed and db connection is being closed");
+// }
+
+// 2.
+
+// let a = prompt("Enter the 1st number?");
+
+// let b = prompt("Enter the 2nd number?");
+
+// if (isNaN(a) || isNaN(b)) {
+//     throw SyntaxError("Invalid input");
+// }
+
+// let sum = parseInt(a) + parseInt(b);
+
+// function main() {
+//     let x = 6;
+//     try {
+//         console.log("Sum is: " + sum * x);
+
+//     } catch (error) {
+//         console.log("Error occured");
+//         // console.log("Error: "+error.message);
+//     } finally {
+//         console.log("Finally files are closed and db connection is being closed");
+//     }
+// }
+
+// let c=main();
