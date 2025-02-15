540fc38366d947257e5ab08ac050dd27d9c72867
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2025-02-16 01:22:01 +0600

    Update classes&object.js

---
+++ classes&object.js
@@ -0,0 +1,69 @@
+// let obj={
+//     a:1,
+//     b:"Ariyan",
+//     c:true
+// };
+
+// console.log(obj);
+
+// let animal={
+//     eats:true
+// };
+
+// let rabbit={
+//     jumps:true
+// };
+
+// rabbit.__proto__=animal;
+
+// console.log(rabbit.eats);
+
+// class Animal{
+//     // constructor(){
+//     //     console.log("Animal created");
+//     // } 
+//     constructor(name){
+//         this.name=name;
+//         console.log("Animal created"+name);
+//     }
+//     eats(){
+//         console.log("Animal eats");
+//     }
+//     jumps(){
+//         console.log("Animal jumps");
+//     }
+// }
+
+// class Lion extends Animal{
+//     constructor(name){
+//         super(name);
+//         console.log("Lion created");
+//     }
+// }
+// let a=new Animal();
+// console.log(a);
+// a.eats();
+// a.jumps();
+
+// let l=new Lion("Shera");
+// console.log(l.name);
+
+
+// 
+// class User{
+//     constructor(name){
+//         this.name=name;
+//     }   
+//     getName(){
+//         return this.name;
+//     }
+//     setName(name){
+//         this.name=name;
+//     }
+// }
+
+// let user = new User("Ariyan");
+// console.log(user.getName());
+
+// user = new User("Fahim");
+// console.log(user.getName());
